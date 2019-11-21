76639069b27fcc3a85219998c0aedd1e5d0419dd
Crystal Quick <cacquick@gmail.com>
2019-11-21 11:45:15 -0600

    Feature update-adding comment section & bug fixes

--- view1.php
+++ view1.php
@@ -1,5 +1,5 @@
 <?php
-      require('database/db_connect.php');
+	    require('database/db_connect.php');
 
       $postsSelect = $db->prepare("SELECT * FROM questions");
       $postsSelect->execute();
@@ -10,32 +10,28 @@
       }
 
 
-
+// this all works
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-  <title>View Recent</title>
-    <link rel="stylesheet" type="text/css" href="styles/view.css">
+	<title>View Recent</title>
+    <!-- <link rel="stylesheet" type="text/css" href="styles/view.css"> -->
 </head>
 <body>
   <?php include('components/navTemp.php'); ?> 
 
   <div id="wrapper">
-<div id="indie">
       <?php foreach($posts as $post): ?>
-      <p><?= $post['title'] ?> <?= htmlspecialchars_decode($post['content']) ?> </p> 
+      <p><?= $post['id'] ?></p>
       <?php  $repliesSelect = $db->prepare("SELECT * FROM reply WHERE postId = :postId"); $repliesSelect->bindValue(':postId', $post['id']); $repliesSelect->execute(); $replies = $repliesSelect->fetchAll(); ?>
       <ul>
         <?php foreach($replies as $reply): ?>
           <li><?= $reply['content'] ?></li>
         <?php endforeach ?>
       </ul>
-    <?php endforeach ?>  
-</div>
-
-
+    <?php endforeach ?>
   </div>
 </body>
 </html> 
